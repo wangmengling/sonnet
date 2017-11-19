88b3f41cdb3d188a4a59b158665a1d4760b51552
王国仲 <294025529@qq.com>
2017-11-19 23:13:24 +0800

    commit
    commit

--- src/DashBoard/DashBoard.js
+++ src/DashBoard/DashBoard.js
@@ -1,15 +1,46 @@
 import React,{ Component } from "react";
-
-
+import NumberCard from "./NumberCard";
+import { Row, Col, Card } from 'antd'
+import { color } from "../../utils/theme";
+import  "./DashBoard.less";
+var numbers =  [
+    {
+      icon: 'pay-circle-o',
+      color: color.green,
+      title: 'Online Review',
+      number: 2781,
+    }, {
+      icon: 'team',
+      color: color.blue,
+      title: 'New Customers',
+      number: 3241,
+    }, {
+      icon: 'message',
+      color: color.purple,
+      title: 'Active Projects',
+      number: 253,
+    }, {
+      icon: 'shopping-cart',
+      color: color.red,
+      title: 'Referrals',
+      number: 4324,
+    },
+  ];
 class DashBoard extends Component {
     constructor(props) {
         super(props);
     }
 
     render() {
+        const numberCards = numbers.map((item, key) => (<Col key={key} lg={6} md={12}>
+            <NumberCard {...item} />
+          </Col>))
         return(
             <div className="DashBoard">
-                Link
+                <div className="DashBoard-NumberCards">
+                    {numberCards}
+                </div>
+                
             </div>
         );
     }
