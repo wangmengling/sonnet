c183b2f17aaf3c96af8cfe6f28865c08d55dd4c4
王国仲 <guo-zhong.wang@reeed.org>
2018-03-05 17:33:37 +0800

    commit
    commit

--- src/Case/CaseDetail.js
+++ src/Case/CaseDetail.js
@@ -1,12 +1,15 @@
 import React,{ Component } from "react";
 import { withRouter,Link } from "react-router-dom";
 import { observer } from "mobx-react";
+import { Card } from 'antd';
 
 class CaseDetail extends Component {
     render(){
         return (
             <div>
-                
+                <Card loading title="Card title" style={{ width: '34%' }}>
+                    Whatever content
+                </Card>
             </div>
         )
     }
